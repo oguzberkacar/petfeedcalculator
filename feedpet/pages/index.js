de4e149502d8ec4bbb51a6a719759dea22c27a5f
Oğuz Berk Açar <94904336+oguzberkacar@users.noreply.github.com>
2022-09-20 16:36:15 +0300

    deleted unneserry pages

--- feedpet/pages/index.js
+++ feedpet/pages/index.js
@@ -8,9 +8,15 @@ export default function Home() {
   const [backHome, setBackHome] = useState("");
   const [mama, setMama] = useState(0);
   const [dogtype, setDogtype] = useState("");
+  const [cattype, setCattype] = useState(0);
+  const [catMama, setCatMama] = useState(0);
+
+  const [catKg,setCatKg]=useState(0);
 
   const dogKgRef = useRef();
   const dogTypeRef = useRef();
+  const catKgRef = useRef();
+  const catTypeRef = useRef();
 
   const handleSelectPet = (e) => {
     setPets(e);
@@ -30,6 +36,11 @@ export default function Home() {
     }
   };
 
+  const catMamaHandler = (e) => {
+    setCatMama(Math.round(70 * Math.pow(e, 0.75) * cattype));
+    setCatKg(e);
+  };
+
   useEffect(() => {
     if (dogtype === "puppy") {
       setMama(dogKgRef.current.value * 2 * 10);
@@ -39,6 +50,12 @@ export default function Home() {
       setMama(0);
     }
   }, [dogtype]);
+
+  useEffect(() => {
+    setCatMama(
+      Math.round(70 * Math.pow(catKg, 0.75) * cattype)
+    );
+  }, [cattype]);
 
   // console.log(dogKgRef.current.value);
 
@@ -61,12 +78,12 @@ export default function Home() {
           <div className="flex flex-col space-y-10 h-screen w-full items-center justify-center ">
             <div className="space-y-4">
               <div>
-                <h1 className="text-center py-2 px-6 rounded-xl text-4xl font-semibold text-white bg-gradient-to-r from-blue-400 to-lime-400">
+                <h1 className="text-center py-2 px-6 mx-3 rounded-xl text-xl sm:text-4xl font-semibold text-white bg-gradient-to-r from-blue-400 to-lime-400">
                   Do you really feed your pet as much as it needs?
                 </h1>
               </div>
               <div className="flex items-center justify-center">
-                <p className="text-center w-fit py-2 px-6 rounded-xl text-2xl font-semibold text-white bg-gradient-to-r from-blue-400 to-lime-400">
+                <p className="text-center w-fit py-2 px-6 rounded-xl text-base sm:text-2xl font-semibold text-white bg-gradient-to-r from-blue-400 to-lime-400">
                   Select your pet first
                 </p>
               </div>
@@ -74,7 +91,7 @@ export default function Home() {
             <div className="flex space-x-8">
               <button
                 onClick={() => handleSelectPet("cat")}
-                className="bg-blue-400 group cursor-pointer text-white w-48 h-48 text-6xl flex flex-col items-center justify-center rounded-full"
+                className="bg-blue-400 group cursor-pointer text-white w-24 h-24 sm:w-48 sm:h-48 text-6xl flex flex-col items-center justify-center rounded-full"
               >
                 <FaCat className="text-lime-400 group-hover:scale-150 transition-all" />
                 <p className="hidden text-4xl mt-3 group-hover:inline-flex transition-all">
@@ -84,7 +101,7 @@ export default function Home() {
 
               <button
                 onClick={() => handleSelectPet("dog")}
-                className="bg-lime-400 group text-white w-48 h-48 text-6xl flex flex-col items-center justify-center rounded-full"
+                className="bg-lime-400 group text-white w-24 h-24 sm:w-48 sm:h-48 text-6xl flex flex-col items-center justify-center rounded-full"
               >
                 <FaDog className="text-blue-400 group-hover:scale-150 transition-all" />
                 <p className="hidden text-4xl mt-3 group-hover:inline-flex transition-all">
@@ -95,30 +112,86 @@ export default function Home() {
           </div>
         )}
         {pets === "cat" && (
-          <div>
-            <div>
-              <button
-                onClick={() => backbutton("cat")}
-                className="top-4 left-4 fixed bg-blue-400 p-3 rounded-full"
-              >
-                <FaArrowLeft />
-              </button>
-            </div>
-            <div className="flex flex-col space-y-10 h-screen w-full items-center justify-center ">
-              deneme
+          <div className="px-10">
+            <button
+              onClick={() => backbutton("cat")}
+              className="top-4 left-4 fixed bg-blue-400 p-2 sm:p-3 rounded-full"
+            >
+              <FaArrowLeft />
+            </button>
+            <div className="flex flex-col sm:flex-row sm:space-x-4  h-screen w-full items-center justify-center ">
+              <div className="w-full  my-32 lg:h-96 border-8 px-5 py-4 border-blue-400 rounded-xl md:w-1/2  flex flex-col items-end justify-center">
+                <div className="w-full">
+                  <label
+                    htmlFor="dogtype"
+                    className="block text-xs font-medium text-gray-700"
+                  >
+                    Select your cat type
+                  </label>
+                  <select
+                    id="dogtype"
+                    name="dogtype"
+                    onChange={(e) => {
+                      setCattype(e.target.value);
+                    }}
+                    className="mt-1 block  w-full rounded-md bg-blue-400 border-gray-300 py-4 pl-3 pr-10 text-2xl focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    defaultValue="none"
+                    ref={catTypeRef}
+                  >
+                    <option value="1">Select Cat Type</option>
+                    <option value="1">Normal</option>
+                    <option value="0.6">Overweight</option>
+                    <option value="1.2">Adult Cats</option>
+                    <option value="1.2">Sterilized Cats</option>
+                    <option value="2.5">For Growing Kittens</option>
+                  </select>
+                </div>
+                <div className="w-full">
+                  <label
+                    htmlFor="kilogram"
+                    className="block text-xs font-medium text-gray-700"
+                  >
+                    Weight of your dog (kg)
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="number"
+                      name="kilogram"
+                      id="kilogram"
+                      ref={catKgRef}
+                      onChange={(e) => catMamaHandler(e.target.value)}
+                      className="block w-full p-4 bg-blue-400 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                      placeholder="X KG"
+                      defaultValue={0}
+                    />
+                  </div>
+                  <div className="text-center  mt-4 space-y-4 text-blue-400 border-4 border-blue-400 p-5 rounded-lg">
+                    <h2 className="lg:text-2xl text-xl">
+                      Daily food of your cat
+                    </h2>
+                    <p className="lg:text-4xl text-2xl">{catMama} Colories</p>
+                    <p className="lg:text-4xl text-2xl">
+                      {Math.round(catMama / 3.5)} Grams
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="hidden xl:w-1/4 lg:flex items-left justify-center">
+                <FaCat className="w-full h-96 bg-lime-400 text-blue-400  p-3" />
+              </div>
             </div>
           </div>
         )}
         {pets === "dog" && (
           <div className="px-10">
             <button
-              onClick={() => backbutton("cat")}
-              className="top-4 left-4 fixed bg-lime-400 p-3 rounded-full"
+              onClick={() => backbutton("dog")}
+              className="top-4 left-4 fixed bg-lime-400 p-2 sm:p-3 rounded-full"
             >
               <FaArrowLeft />
             </button>
             <div className="flex flex-col sm:flex-row sm:space-x-4  h-screen w-full items-center justify-center ">
-              <div className="w-full h-96 border-8 px-5 py-4 border-lime-400 rounded-xl sm:w-1/2 lg:w-1/4 flex flex-col items-end justify-center">
+              <div className="w-full  my-32 lg:h-96 border-8 px-5 py-4 border-lime-400 rounded-xl md:w-1/2  flex flex-col items-end justify-center">
                 <div className="w-full">
                   <label
                     htmlFor="dogtype"
@@ -144,7 +217,7 @@ export default function Home() {
                 <div className="w-full">
                   <label
                     htmlFor="kilogram"
-                    className="block text-xl font-medium text-gray-700"
+                    className="block text-xs font-medium text-gray-700"
                   >
                     Weight of your dog (kg)
                   </label>
@@ -160,19 +233,26 @@ export default function Home() {
                       defaultValue={0}
                     />
                   </div>
-                  <div className="text-center  mt-4 space-y-4 text-white border-4 border-lime-400 p-5 rounded-lg">
-                    <h2 className="text-2xl ">Daily food of your dog</h2>{" "}
-                    <p className="text-4xl">{mama} gram</p>
+                  <div className="text-center  mt-4 space-y-4 text-lime-400 border-4 border-lime-400 p-5 rounded-lg">
+                    <h2 className="lg:text-2xl text-xl">
+                      Daily food of your dog
+                    </h2>
+                    <p className="lg:text-4xl text-2xl">{mama} gram</p>
                   </div>
                 </div>
               </div>
-              <div className="hidden lg:w-1/4 border-lime-400 lg:flex items-left justify-center">
-                <FaDog className="w-full h-96 border-2 bg-lime-400 text-blue-400 border-lime-400 rounded-xl p-3" />
+              <div className="hidden xl:w-1/4 border-lime-400 lg:flex items-left justify-center">
+                <FaDog className="w-full h-96  text-lime-400  p-3" />
               </div>
             </div>
           </div>
         )}
-        <div className="w-full fixed bottom-0 h-10 flex items-center justify-center  text-xl bg-black text-white">This site made by Oğuz Berk Açar for demo purposes.</div>
+        <div className="w-full fixed bottom-10 h-10 flex text-center items-center justify-center text-xs sm:text-xl custombackreverse text-black">
+          Theese values are just suggestions, please consult your vet.
+        </div>
+        <div className="w-full fixed bottom-0 h-10 flex text-center items-center justify-center text-xs sm:text-xl bg-black text-white">
+          This site made by Oğuz Berk Açar for demo purposes.
+        </div>
       </div>
     </div>
   );
